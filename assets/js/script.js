3b253f0933e1ea42ce0f48342e7d8d2f3ccdd6b3
Nicolas Yanase <142536049+Nick-Yanase@users.noreply.github.com>
2024-03-04 00:50:03 -0300

    commit files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
-const mode = document.getElementsById("mode_icon");
+/*//criar uma variavel para o botão da lua e realizar as ações co ele
+const mode = document.getElementsByClassName("mode_icon");
+const form = Document.getElement;
+//SE A VARIAVEL MODE FOR = MODE_ICON FAÇA, evento de click
+mode.addEventListener("click", () => {
+  const form = document.getElementById("login_form");
+  if (mode.classList.constains("fa-moon")) {
+    //caso tenha a lua ou o icone da lua
+    mode.classList.remove("fa-moon");
+    mode.classList.add("fa-sun");
 
-//SE A VARIAVEL MODE FOR = MODE_ICON FAÇA
-mode.addEventListener('click', () =>{
-  if(mode.classList.constains('fa-moon')){
-    mode.classList.remove('fa-moon');
-    mode.classList.add('fa-sun');
-
-    return
+    form.classList.add("dark");
+    return;
   }
 
-  mode.classList.add('fa-moon');
-  mode.classList.remove('fa-sun');
+  mode.classList.add("fa-moon");
+  mode.classList.remove("fa-sun");
+  form.classList.remove("dark");
+});*/
+
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('login_form');
+
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark');
+        return ;
+    }
+    
+    mode.classList.remove('fa-sun');
+    mode.classList.add('fa-moon');
+
+    form.classList.remove('dark');
 });
